966dfc95d9d9159ef9d758c1b48142bc7058343e
Thomas Osborn <trosborn@gmail.com>
2014-01-28 17:53:03 -0800

    Fixes #14 #15 #16 #17

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Portfolio::Application.routes.draw do
+  resources :posts
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
-feature "CanAccessHome" do
+feature "can access home" do
   scenario "the test is sound" do
     visit root_path
     page.must_have_content "Welcome"
     page.wont_have_content "Goobye All!"
   end
 end
-
-#yolo
