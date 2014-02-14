be3aa94b65ceeb0c25a135ebaa1cb3a784cfe366
Thomas Osborn <trosborn@gmail.com>
2014-02-14 12:31:08 -0800

    encrypts heroku api

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 feature "can access home" do
   scenario "the test is sound" do
     visit root_path
-    page.must_have_content "Welcome"
+    page.must_have_content "Thomas"
     page.wont_have_content "Goobye All!"
   end
 end
