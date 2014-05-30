1d3210680d23ec898dbf88738cbac6a442704f96
Thomas Osborn <trosborn@gmail.com>
2014-05-30 11:58:07 -0700

    maybe actually fix the yaml problem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',
     aws_access_key_id:     ENV['S3_KEY'],
-    aws_Secret_access_key: ENV['S3_SECRET'],
+    aws_secret_access_key: ENV['S3_SECRET'],
 
   }
 
