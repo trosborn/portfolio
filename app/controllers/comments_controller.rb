804c310cdcfaf4ba87faeffdf2b063f94ed7ef68
Thomas Osborn <trosborn@gmail.com>
2014-06-03 15:08:11 -0700

    cleans up extra files

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
     @comment = @commentable.comments.new(comment_params)
     if @comment.save
       redirect_to @commentable, notice: "Comment is awaiting moderation"
-      CommentMailer.delay_for(2.minutes).comment_new(@comment, @commentable)
+      CommentMailer.delay_for(2.minutes).new_comment(@comment, @commentable)
     else
       instance_variable_set(("@#{@resource.singularize}").to_sym, @commentable)
       render template: "#{@resource}/show"
--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,7 +1,7 @@
 class CommentMailer < ActionMailer::Base
   default from: "tosborn@u.washington.edu"
 
-  def comment_new comment, commentable
+  def new_comment comment, commentable
     @comment = comment
     @commentable = commentable
     mail to: 'tosborn@u.washington.edu', subject: "yolo"
