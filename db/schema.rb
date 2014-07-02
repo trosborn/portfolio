673d74eb73a82a8d069bb282ecdff8eea572b1ec
Thomas Osborn <trosborn@gmail.com>
2014-07-01 17:05:07 -0700

    rm merge conflict in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140529032352) do
-=======
-ActiveRecord::Schema.define(version: 20140521003506) do
->>>>>>> master
 
   create_table "comments", force: true do |t|
     t.string   "author"
