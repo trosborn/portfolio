8daef1075f34b84d2190fcd0a4158fca688e13cd
Thomas Osborn <trosborn@gmail.com>
2014-09-09 19:15:34 -0700

    sets up production.rb for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,5 +14,18 @@ Portfolio::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => 'http://www.trosborn.com' }
+  config.action_mailer.smtp_settings = {
+    address: “smtp.gmail.com”,
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: “plain”,
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
+
+
