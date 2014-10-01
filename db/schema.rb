561aff07459e4f0152555aa795096b8122d22c53
trosborn <trosborn@gmail.com>
2014-10-01 15:24:02 -0700

    beginning of foundation top bar

--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 20140529032352) do
     t.string   "provider"
     t.string   "uid"
     t.string   "username"
-    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
