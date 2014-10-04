46f2b9edc4e78e4281d72ba965dee71b98151b60
trosborn <trosborn@gmail.com>
2014-10-04 13:26:56 -0700

    completes parallax on blog index, and other minor front end changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :preview]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -33,9 +33,7 @@ class PostsController < ApplicationController
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
-
     current_user.posts << @post
-
   end
 
   def update
