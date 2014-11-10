176d9203e4bb1d91d98cbbd43bf42214a12301a5
trosborn <trosborn@gmail.com>
2014-11-09 22:11:00 -0800

    fixes crud images test

--- test/features/images/can_CRUD_images_test.rb
+++ test/features/images/can_CRUD_images_test.rb
@@ -6,7 +6,4 @@ feature "image CRUD" do
     image = Image.create!(body: fixture_file_upload('files/palestinian_resistance_wall.jpg', 'image/jpg'))
     assert(File.exists?(image.reload.body.file.path))
   end
-  scenario "Joe Schmoe cannot upload a new image"
-
-  end
 end
