11a43799d43a9775575814dda28a7e2d9d141d17
Thomas Osborn <trosborn@gmail.com>
2015-01-07 17:26:02 +1200

    add ruby code to posts seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 5.times do |i|
-  Post.create(title: "Post #{i.to_s + "Aminoize"}", body: "Bacon ipsum dolor sit amet pork chop shank strip steak. Tongue ham kevin shoulder kielbasa venison bacon short ribs t-bone capicola short loin. Prosciutto tenderloin meatloaf shoulder. Filet mignon strip steak bresaola, drumstick jowl pig swine short ribs tenderloin flank brisket. Sausage hamburger pork loin tongue, pancetta spare ribs bresaola ham doner ribeye shoulder tenderloin fatback. Leberkas boudin pastrami pork chop, chuck shoulder pork loin filet mignon capicola kevin beef ribs porchetta chicken. #{i}")
+  Post.create(title: "Post #{i.to_s + "Aminoize"}", body: "Bacon ipsum dolor sit amet pork chop shank strip steak. Tongue ham kevin shoulder kielbasa venison bacon short ribs t-bone capicola short loin. Prosciutto tenderloin meatloaf shoulder. Filet mignon strip steak bresaola, drumstick jowl pig swine short ribs tenderloin flank brisket. Sausage hamburger pork loin tongue, pancetta spare ribs bresaola ham doner ribeye shoulder tenderloin fatback. Leberkas boudin pastrami pork chop, chuck shoulder pork loin filet mignon capicola kevin beef ribs porchetta chicken. #{i} ````ruby
+    def yolo
+    end")
 end
