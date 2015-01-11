c742ecfab7ed2f715934bc79af3b7fd62cbee5f5
Thomas Osborn <trosborn@gmail.com>
2015-01-10 16:26:43 -0800

    remove code lines

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
 class CodeRayify < Redcarpet::Render::HTML
   def block_code(code, language)
-    CodeRay.scan(code, language).div(:line_numbers => :table)
+    CodeRay.scan(code, language).div
   end
 end
 
