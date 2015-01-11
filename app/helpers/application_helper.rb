c5c28311eeaa81a1f1d635c7ede74d46f7eb78dd
Thomas Osborn <trosborn@gmail.com>
2015-01-10 16:21:09 -0800

    set coderay line numbers back to true

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
 class CodeRayify < Redcarpet::Render::HTML
   def block_code(code, language)
-    CodeRay.scan(code, language).div(:line_numbers => :nil)
+    CodeRay.scan(code, language).div(:line_numbers => :table)
   end
 end
 
