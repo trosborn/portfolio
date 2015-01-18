c4c43b138e29e4363ace6219548634af326492e1
Thomas Osborn <trosborn@gmail.com>
2015-01-17 22:20:26 -0800

    drops image from local projects table, then adds it back with migration, hopefully will fix all issues once run on heroku

--- db/migrate/20150117223915_add_url_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddUrlToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :url, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117230557) do
+ActiveRecord::Schema.define(version: 20150118061937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,9 +59,9 @@ ActiveRecord::Schema.define(version: 20150117230557) do
     t.string   "technologies_used"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
     t.string   "url"
     t.string   "description"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
